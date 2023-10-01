1b1747ca0010e755c6d552de4a224cd955eddfb7
Alvin Alaphat <alvinalaphat@Alvins-MacBook-Air-2.local>
2023-10-01 19:58:34 +0100

    rate limiter finished

--- test.py
+++ test.py
@@ -8,10 +8,10 @@ def client():
         yield client
 
 def test_image_rate_limit(client):
-    endpoint = "http://127.0.0.1:5000/download/Profile2.jpg"
+    endpoint = "http://127.0.0.1:5000/download/test.py"
     
     # Send requests exceeding the rate limit
-    for _ in range(11):  # Assuming the rate limit is set to 10 requests per minute
+    for _ in range(16):  # Assuming the rate limit is set to 10 requests per minute
         response = requests.get(endpoint)
         assert response.status_code == 429  # 429 Too Many Requests
 
